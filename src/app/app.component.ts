263d9133a93203d6c524eea32930bda19c0b216d
Sushant Sharma <ssharma@argusoft.com>
2023-04-25 18:57:36 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+ 
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username' : new FormControl(null,Validators.required),
+        'email': new FormControl(null,[Validators.required, Validators.email])
+      }),
+     
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+
+    });
+  }
+
+// no need of local refernce like in template driven
+  onSubmit(){
+    console.log(this.signupForm);
+   
+
+  }
+  onAddHobby(){
+    const control = new FormControl(null,Validators.required);
+    ((<FormArray>this.signupForm.get('hobbies')).push(control));
+  }
+}
