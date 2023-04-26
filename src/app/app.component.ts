a6d9fd66a811f8af06e9c3b5316e1f05a5ad03f3
Sushant Sharma <ssharma@argusoft.com>
2023-04-26 12:06:44 +0530

    full forms feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,28 +10,68 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
  
   genders = ['male', 'female'];
+  forbiddenUsernames = ['Chris', 'Anna'];
   signupForm: FormGroup;
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username' : new FormControl(null,Validators.required),
-        'email': new FormControl(null,[Validators.required, Validators.email])
+        'username' : new FormControl(null,[Validators.required, this.forbiddenNames]),
+        'email': new FormControl(null,[Validators.required, Validators.email],this.forbiddenEmails)
       }),
      
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
 
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value)=>{console.log(value);}
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (value)=>{console.log(value);}
+    );
+    this.signupForm.setValue({
+      userData: {
+        username : 'Sushant',
+        email: 'ssharma@argusoft.com'
+      },
+     
+      gender: 'male',
+      hobbies: []
+
+    })
   }
 
 // no need of local refernce like in template driven
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
    
 
   }
   onAddHobby(){
     const control = new FormControl(null,Validators.required);
     ((<FormArray>this.signupForm.get('hobbies')).push(control));
+  }
+
+ forbiddenNames = (control: FormControl):{[s:string]:boolean }=>{
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return  null;
+  }
+
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject)=>{
+      setTimeout(()=>{
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true});
+        }
+        else {
+          resolve(null);
+        }
+      },1500);
+    });
+    return promise;
   }
 }
